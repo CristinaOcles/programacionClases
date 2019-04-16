bce9566d57633ef49fa8ac1f5dc4d6a54f3e5e95
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2019-04-16 16:12:21 -0300

    Add files via upload

---
+++ validaciones/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utn.h"
+
+int main()
+{
+   // char auxName[500] = "test";
+   // char auxNumber[500] = "333";
+    char auxDni [500] = "dni";
+
+  /*  if(getName( "Nombre?: ",
+                "Error",
+                2,
+                5,
+                2,
+                auxName) == 0)
+    {
+        printf("OK: %s",auxName);
+    }
+    else
+    {
+        printf("ER: %s",auxName);
+    }
+*/
+
+/*
+    if(getNumber( "numero?: ",
+                "Error",
+                1,
+                150,
+                2,
+                auxNumber) == 0)
+    {
+        printf("OK: %s",auxNumber);
+    }
+    else
+    {
+        printf("ER: %s",auxNumber);
+    }
+
+
+*/
+
+
+
+        if(getDni( "numero de dni?: ",
+                "Error",
+                999999,
+                60000000,
+                2,
+                auxDni) == 0)
+    {
+        printf("OK: %s",auxDni);
+    }
+    else
+    {
+        printf("ER: %s",auxDni);
+    }
+
+
+    return 0;
+}
---
+++ validaciones/utn.h
@@ -0,0 +1,51 @@
+#define TRUE 1
+#define FALSE 0
+
+int getString ( char* msg,
+                char* msgError,
+                int minimo,
+                int maximo,
+                int reintentos,
+                char* resultado);
+//-------------------------------------
+
+int getName (   char* msg,
+                char* msgError,
+                int minimo,
+                int maximo,
+                int reintentos,
+                char* resultado);
+
+
+int isValidName (char* cadena);
+
+
+
+
+
+
+int getNumber (   char* msg,
+                char* msgError,
+                int minimo,
+                int maximo,
+                int reintentos,
+                char* resultado);
+
+
+int isValidNumber(char* cadena);
+
+
+
+
+
+
+
+int getDni (   char* msg,
+                char* msgError,
+                int minimo,
+                int maximo,
+                int reintentos,
+                char* resultado);
+
+
+int isValidDni(char* cadena);
