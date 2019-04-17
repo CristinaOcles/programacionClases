056fbc0d918c7e2266c3d2e71e2e3793ecfe2d35
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2019-04-16 21:35:50 -0300

    clase 8

---
+++ Nueva carpeta/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utn.h"
+#define QTY_EMPLEADOS 50
+
+
+
+
+int main()
+{
+   // char auxName[500] = "test";
+    int indice;
+
+
+    char arrayNombres[QTY_EMPLEADOS][50];
+
+    initName(arrayNombres,QTY_EMPLEADOS);
+
+    strncpy(arrayNombres[0], "Juan", 50);
+    strncpy(arrayNombres[1], "Pedro", 50);
+    strncpy(arrayNombres[2], "Luis", 50);
+
+    //mostrarNombres(arrayNombres, 6);
+
+    indice = buscarIndiceVacio(arrayNombres, QTY_EMPLEADOS);
+
+    mostrarNombres(arrayNombres, QTY_EMPLEADOS);
+
+    /*
+
+    if(getName( "Nombre?: ",
+                "Error",
+                2,
+                5,
+                2,
+                auxName) == 0)
+    {
+        printf("OK: %s",auxName);
+    }
+    else
+    {
+        printf("ER: %s",auxName);
+    }
+    */
+
+
+    return 0;
+}
+
+
+
---
+++ Nueva carpeta/utn.h
@@ -0,0 +1,27 @@
+#define TRUE 1
+#define FALSE 0
+
+int getString ( char* msg,
+                char* msgError,
+                int minimo,
+                int maximo,
+                int reintentos,
+                char* resultado);
+//-------------------------------------
+
+int getName (   char* msg,
+                char* msgError,
+                int minimo,
+                int maximo,
+                int reintentos,
+                char* resultado);
+
+
+int isValidName (char* cadena);
+
+
+int mostrarNombres(char cadena[][50], int largo);
+void initName(char cadena[][50], int largo);
+int buscarIndiceVacio(char cadena[][50], int largo);
+
+
